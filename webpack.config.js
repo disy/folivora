6de4836b63f40f84e76f7260a75ea28854b66e9e
sualko <klaus@jsxc.org>
2018-11-08 13:36:20 +0100

    use sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './client/index.ts',
+    entry: ['./client/index.ts', './styles/index.scss'],
     module: {
         rules: [{
-            test: /\.ts?$/,
+            test: /\.ts$/,
             use: 'ts-loader',
             exclude: /node_modules/
-        }]
+        }, {
+            test: /\.scss$/,
+            use: [
+               MiniCssExtractPlugin.loader, {
+                  loader: 'css-loader',
+                  options: {
+                     url: false
+                  }
+               },
+               'sass-loader'
+            ]
+         }]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -15,5 +27,10 @@ module.exports = {
     output: {
         filename: 'scripts/bundle.js',
         path: path.resolve(__dirname, 'public')
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles/bundle.css',
+         })
+    ]
 };
